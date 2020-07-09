43c1a35daa5aaf07b24f0063b4d5a034f21292d1
Greg Mazenko <bizcorr9586@att.net>
2020-07-09 10:04:46 -0500

    created package myioutils

--- src/test/java/MyIOUtilsTest.java
+++ src/test/java/MyIOUtilsTest.java
@@ -1,3 +1,4 @@
+import myioutils.MyIOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ public class MyIOUtilsTest {
         MyIOUtils.printlnBlueText("This is Blue text");
 
         MyIOUtils myIOUtils = new MyIOUtils();
-        Path path = myIOUtils.getResFilePath("sample_string_input.txt");
+        Path path = myIOUtils.getResFilePath("../sample_string_input.txt");
         String expectedPath = "/home/greg/javaProjects/MyIOUtils/target/classes/sample_string_input.txt";
         boolean pathMatches = expectedPath.equals(path.toString());
         Assertions.assertTrue(pathMatches, "Input file path does not match expected path");
